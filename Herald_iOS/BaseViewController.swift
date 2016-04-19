62c88f843a1fdbefea27bf8e3854f70b12c9a757
rikumi <riku.mi@outlook.com>
2016-04-19 13:44:20 +0800

    完成一卡通模块

--- Herald_iOS/BaseViewController.swift
+++ Herald_iOS/BaseViewController.swift
@@ -17,7 +17,10 @@ class BaseViewController : UIViewController {
     var alertDialog : MBProgressHUD?
     
     func showProgressDialog () {
-        progressDialog = MBProgressHUD.showHUDAddedTo(view, animated: true)
+        hideProgressDialog()
+        progressDialog = MBProgressHUD(view: view)
+        UIApplication.sharedApplication().delegate?.window!!.addSubview(progressDialog!)
+        progressDialog?.show(true)
         progressDialog?.labelText = "请稍候…"
     }
     
@@ -26,10 +29,33 @@ class BaseViewController : UIViewController {
     }
     
     func showMessage (message : String) {
-        alertDialog = MBProgressHUD.showHUDAddedTo(view, animated: true)
+        alertDialog = MBProgressHUD(view: view)
+        UIApplication.sharedApplication().delegate?.window!!.addSubview(alertDialog!)
+        alertDialog?.show(true)
         alertDialog?.labelText = message
         alertDialog?.mode = .Text
-        alertDialog?.minShowTime = 2
+        alertDialog?.minShowTime = 1
         alertDialog?.hide(true)
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        hideProgressDialog()
+    }
+    
+    func showTipDialogIfUnknown (message: String, cachePostfix: String, runAfter: () -> Void) {
+        let shown = CacheHelper.getCache("tip_ignored_" + cachePostfix) == "1"
+        if !shown {
+            let dialog = UIAlertController(title: "提示", message: message, preferredStyle: UIAlertControllerStyle.Alert)
+            dialog.addAction(UIAlertAction(title: "确定", style: UIAlertActionStyle.Default){
+                (action: UIAlertAction) -> Void in runAfter()})
+            dialog.addAction(UIAlertAction(title: "不再提示", style: UIAlertActionStyle.Cancel){
+                (action: UIAlertAction) -> Void in
+                CacheHelper.setCache("tip_ignored_" + cachePostfix, cacheValue: "1")
+                runAfter()
+            })
+            presentViewController(dialog, animated: true, completion: nil)
+        } else {
+            runAfter()
+        }
     }
 }
--- Herald_iOS/CardViewController.swift
+++ Herald_iOS/CardViewController.swift
@@ -8,33 +8,104 @@
 
 import Foundation
 import UIKit
+import SwiftyJSON
 
 class CardViewController : BaseViewController, UITableViewDelegate, UITableViewDataSource {
     
+    @IBOutlet var tableView : UITableView?
     
     override func viewDidLoad() {
         let cache = CacheHelper.getCache("herald_card")
         if cache != "" {
             loadCache()
         } else {
-            //refreshCache()
+            refreshCache()
         }
     }
     
+    var history : [[CardHistoryModel]] = []
+    
     func loadCache() {
         let cache = CacheHelper.getCache("herald_card")
-        
+        if cache != "" {
+            let jsonCache = JSON.parse(cache)["content"]
+            let jsonArray = jsonCache["detial"]
+            let extra = jsonCache["left"].string
+            if extra != nil {
+                title = "余额：" + extra!
+            } else {
+                title = "一卡通"
+                showMessage("解析失败，请手动刷新")
+            }
+            
+            history.removeAll()
+            var lastDate = ""
+            for i in 0 ..< jsonArray.count {
+                let obj = jsonArray[i]
+                guard let datetimeStr = obj["date"].string else { continue }
+                let date = datetimeStr.componentsSeparatedByString(" ")[0]
+                let time = datetimeStr.componentsSeparatedByString(" ")[1]
+                guard let place = obj["system"].string else { continue }
+                guard let type = obj["type"].string else { continue }
+                guard let cost = obj["price"].string else { continue }
+                guard let left = obj["left"].string else { continue }
+                if date != lastDate {
+                    history.append([])
+                    lastDate = date
+                }
+                let newElement = CardHistoryModel(date: date, time: time, place: place, type: type, cost: cost, left: left)
+                guard var lastSection = history.last else { continue }
+                history.removeLast()
+                lastSection.append(newElement)
+                history.append(lastSection)
+            }
+        }
+    }
+    
+    @IBAction func refreshCache () {
+        showProgressDialog()
+        ApiRequest().api("card").uuid().post("timedelta", "31")
+            .toCache("herald_card") {json -> String in
+                guard let str = json.rawString() else {return ""}
+                return str
+            }
+            .onFinish { success, _, _ in
+                self.hideProgressDialog()
+                if success {
+                    self.loadCache()
+                    self.showMessage("刷新成功")
+                }
+            }.run()
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return history[section].count
+    }
+    
+    func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return history[section][0].date
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        return tableView.dequeueReusableCellWithIdentifier("CardTableViewCell", forIndexPath: indexPath)
+        let cell = tableView.dequeueReusableCellWithIdentifier("CardTableViewCell", forIndexPath: indexPath) as! CardTableViewCell
+        
+        let model = history[indexPath.section][indexPath.row]
+        cell.time?.text = model.time
+        cell.place?.text = model.place
+        cell.type?.text = model.type
+        cell.cost?.text = model.cost
+        cell.left?.text = model.left
+        return cell
     }
     
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 0
+        return history.count
+    }
+    
+    @IBAction func goToChargePage () {
+        showTipDialogIfUnknown("注意：由于一卡通中心配置问题，充值之后需要刷卡消费一次，一卡通余额才能正常显示哦", cachePostfix: "card_charge") {
+            () -> Void in
+                UIApplication.sharedApplication().openURL(NSURL(string: "http://58.192.115.47:8088/wechat-web/login/initlogin.html")!)
+        }
     }
 }
