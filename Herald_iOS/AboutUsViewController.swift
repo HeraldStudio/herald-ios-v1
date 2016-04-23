106eae006ecb154d6245711ec4d5e123d4392982
rikumi <vhyme@live.cn>
2016-04-23 19:17:45 +0800

    实现了关于页面 调整图书馆绑定对话框的样式

---
+++ Herald_iOS/AboutUsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  AboutUsViewController.swift
+//  Herald_iOS
+//
+//  Created by 于海通 on 16/4/23.
+//  Copyright © 2016年 于海通. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AboutUsViewController : BaseTableViewController {
+    
+    override func viewDidLoad() {
+        tableView.estimatedRowHeight = 64;
+        tableView.rowHeight = UITableViewAutomaticDimension;
+    }
+}
