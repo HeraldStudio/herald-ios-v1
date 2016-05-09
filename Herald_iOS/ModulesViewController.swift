c24196bfabca3c23ac59dadd216f62ad7031dd3e
zzleft <left.in@qq.com>
2016-05-09 13:29:53 +0800

    加入简单的3D Touch 存在问题： 1.尚不明确每次按压之后，出现cell的复制层 2.主页面按压的菜单选项未加入图标。要求35*35的单色像素 3.由于课表详情页是写死宽度的，所以直接取消了按压预览 4.网页内嵌页的预览暂时没做

--- Herald_iOS/ModulesViewController.swift
+++ Herald_iOS/ModulesViewController.swift
@@ -15,10 +15,12 @@ class ModulesViewController: UIViewController, UITableViewDelegate, UITableViewD
         let th = (tabBarController?.tabBar.frame.height)!
         let bottomPadding = UIView(frame: CGRect(x: 0.0, y: 0.0, width: tw, height: th))
         moduleTableView.tableFooterView = bottomPadding
+        print("viewDidLoad")
     }
     
     override func viewDidAppear(animated: Bool) {
         setupModuleList()
+        print("viewDidAppear")
     }
     
     override func didReceiveMemoryWarning() {
@@ -62,11 +64,21 @@ class ModulesViewController: UIViewController, UITableViewDelegate, UITableViewD
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let moduleCell = moduleTableView.dequeueReusableCellWithIdentifier("ModuleTableViewCell", forIndexPath: indexPath) as! ModuleTableViewCell
         
+        //cell注册3D touch代理
+        if #available(iOS 9.0, *) {
+            if traitCollection.forceTouchCapability == .Available {
+                self.registerForPreviewingWithDelegate(self, sourceView: moduleCell)
+            }
+        }
+        
         let module = sections[indexPath.section][indexPath.row]
         
         moduleCell.icon.image = UIImage(named: module.icon)
         moduleCell.label.text = module.nameTip
         moduleCell.detail.text = module.desc
+        
+        moduleCell.selectionStyle = UITableViewCellSelectionStyle.None
+        
         return moduleCell
     }
     
@@ -81,3 +93,41 @@ class ModulesViewController: UIViewController, UITableViewDelegate, UITableViewD
         sections[indexPath.section][indexPath.row].open(navigationController!)
     }
 }
+
+
+//3d touch遵守协议
+extension ModulesViewController:UIViewControllerPreviewingDelegate {
+    
+    //peek
+    @available(iOS 9.0, *)
+    func previewingContext(previewingContext: UIViewControllerPreviewing, viewControllerForLocation location: CGPoint) -> UIViewController? {
+
+        let cell = previewingContext.sourceView as! ModuleTableViewCell
+        
+        //通过触摸点的位置获取的是相对于cell的坐标，需要转换为moduleTableView的，然后获得indexPath
+        let tableViewLocation = cell.convertPoint(location, toView: moduleTableView)
+        let indexPath = moduleTableView.indexPathForRowAtPoint(tableViewLocation)
+
+        previewingContext.sourceRect = cell.bounds
+        
+        if cell.label!.text == "课表助手" || cell.label!.text == "模块管理" {
+            return nil
+        }
+        
+        if sections[indexPath!.section][indexPath!.row].controller.hasPrefix("http") {
+            return nil
+        }else {
+            let detailVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier(sections[indexPath!.section][indexPath!.row].controller)
+            detailVC.preferredContentSize = CGSizeMake(SCREEN_WIDTH, 600)
+            return detailVC
+        }
+    }
+    
+    //pop
+    func previewingContext(previewingContext: UIViewControllerPreviewing, commitViewController viewControllerToCommit: UIViewController) {
+        showViewController(viewControllerToCommit, sender: self)
+    }
+}
+
+//屏幕宽度
+let SCREEN_WIDTH = UIScreen.mainScreen().bounds.size.width
