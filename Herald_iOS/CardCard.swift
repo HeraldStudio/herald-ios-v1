9fe08d3ded833b61c3ee7352dc04414372f3d190
rikumi <vhyme@live.cn>
2016-05-31 22:15:13 +0800

    去除余额的刷新，与当天消费合并 修正某些情况下余额超过1000元会导致一卡通模块解析失败的问题

--- Herald_iOS/CardCard.swift
+++ Herald_iOS/CardCard.swift
@@ -12,14 +12,14 @@ import SwiftyJSON
 class CardCard {
     
     static func getRefresher () -> [ApiRequest] {
-        return [ApiRequest().api("card").uuid().toCache("herald_card_left")]
+        return [ApiRequest().api("card").uuid().post("timedelta", "1").toCache("herald_card_today")]
     }
     
     static func getCard() -> CardsModel {
-        let cache = CacheHelper.get("herald_card_left")
+        let cache = CacheHelper.get("herald_card_today")
         let content = JSON.parse(cache)["content"]
         // 获取余额并且设置
-        if let extra = Float(content["left"].stringValue) {
+        if let extra = Float(content["cardLeft"].stringValue.replaceAll(",", "")) {
             if extra < 20 {
                 let model = CardsModel(cellId: "CardsCellCard", module: R.module.card, desc: "一卡通余额还有\(String(format: "%.2f", extra))元，快点我充值~\n如果已经充值过了，需要刷卡消费一次才会更新哦~", priority: .CONTENT_NOTIFY)
                 model.rows[0].destination = CardViewController.url
