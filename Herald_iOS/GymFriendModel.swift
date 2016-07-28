aadc2a576712e3302a271a6cf2efb88f0a8f9be4
rikumi <riku.mi@outlook.com>
2016-07-28 14:56:29 +0800

    去除场馆预约好友列表缓存键名中的用户名

--- Herald_iOS/GymFriendModel.swift
+++ Herald_iOS/GymFriendModel.swift
@@ -14,14 +14,14 @@ class GymFriendModel {
     
     static var friendCache : [JSON] {
         get {
-            var cache = CacheHelper.get("herald_gymreserve_friend_\(ApiHelper.currentUser.userName)")
+            var cache = CacheHelper.get("herald_gymreserve_friend")
             if cache == "" {
                 cache = "[]"
             }
             return JSON.parse(cache).arrayValue
         } set (value) {
             let str = JSON(value).rawString()
-            CacheHelper.set("herald_gymreserve_friend_\(ApiHelper.currentUser.userName)", str == nil ? "" : str!)
+            CacheHelper.set("herald_gymreserve_friend", str == nil ? "" : str!)
         }
     }
     
