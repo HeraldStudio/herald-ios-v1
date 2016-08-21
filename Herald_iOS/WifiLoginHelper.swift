b8226fdd3e250ecbf39a6122b5860ab8e8c6e44d
rikumi <vhyme@live.cn>
2016-08-21 09:33:01 +0800

    调整校园网快捷登录功能文案

--- Herald_iOS/WifiLoginHelper.swift
+++ Herald_iOS/WifiLoginHelper.swift
@@ -27,10 +27,8 @@ class WifiLoginHelper {
             if WifiLoginHelper.working { return }
             WifiLoginHelper.working = true
             
-            //vc.showTipDialogIfUnknown("注意：请先进入 [设置]-[Wi-Fi]-\"seu-wlan\" 右侧的 [i] 按钮，关闭 [自动连接]，才能正常使用~", cachePostfix: "wifi") {
             self.vc.showProgressDialog()
             self.beginCheck()
-            //}
         }
     }
     
@@ -42,7 +40,7 @@ class WifiLoginHelper {
             } else {
                 self.vc.hideProgressDialog()
                 WifiLoginHelper.working = false
-                self.vc.showMessage("校园网状态异常，请先手动连接到 seu-wlan，并等待网络图标变成 Wi-Fi 图标之后再试~\n\n如果系统弹出登录页面，请到 Wi-Fi 设置中关闭 seu-wlan 的 [自动登录] 功能再试~")
+                self.vc.showMessage("校园网快捷登录：状态异常，请先手动连接到 seu-wlan，并等待网络图标变成 Wi-Fi 图标之后再试~\n\n如果系统弹出登录页面，请到 Wi-Fi 设置中关闭 seu-wlan 的 [自动登录] 功能再试~")
             }
         }.runWithoutFatalListener()
     }
@@ -54,23 +52,24 @@ class WifiLoginHelper {
                 let responseJSON = JSON.parse(response)
                 
                 if responseJSON["status"].intValue == 0 {
-                    // 未登录状态，直接登录
-                    //self.vc.showMessage("摇一摇：未登录状态，正在尝试登录~")
+                    
                     self.loginToService()
                     
                 } else if responseJSON["logout_username"].stringValue != ApiHelper.getWifiUserName() {
-                    // 已登录，但账号与当前设置的账号不同
-                    //self.vc.showMessage("摇一摇：已登录其它账号，正在尝试退出~")
+                    
                     self.logoutThenLogin()
+                    
                 } else {
+                    
                     self.vc.hideProgressDialog()
                     WifiLoginHelper.working = false
-                    self.vc.showMessage("已登录校园网，无需重复登录~")
+                    self.vc.showMessage("校园网快捷登录：已登录状态，无需重复登录~")
+                    
                 }
             } else {
                 self.vc.hideProgressDialog()
                 WifiLoginHelper.working = false
-                self.vc.showMessage("校园网信号不佳，换个姿势试试？")
+                self.vc.showMessage("校园网快捷登录：信号不佳，换个姿势试试？")
             }
         }.run()
     }
@@ -83,7 +82,7 @@ class WifiLoginHelper {
             } else {
                 self.vc.hideProgressDialog()
                 WifiLoginHelper.working = false
-                self.vc.showMessage("已登录账号退出失败，请重试~")
+                self.vc.showMessage("校园网快捷登录：已登录账号退出失败，请重试~")
             }
         }.run()
     }
@@ -102,16 +101,16 @@ class WifiLoginHelper {
                 if success {
                     let info = JSON.parse(response)
                     if info["status"].intValue == 1 {
-                        self.vc.showMessage("小猴登录校园网成功~")
+                        self.vc.showMessage("校园网快捷登录成功~")
                     } else {
                         if let error = info["info"].string {
-                            self.vc.showMessage("登录失败：\(error)")
+                            self.vc.showMessage("校园网快捷登录失败：\(error)")
                         } else {
-                            self.vc.showMessage("登录失败，出现未知错误")
+                            self.vc.showMessage("校园网快捷登录：信号不佳，换个姿势试试？")
                         }
                     }
                 } else {
-                    self.vc.showMessage("校园网信号不佳，换个姿势试试？")
+                    self.vc.showMessage("校园网快捷登录：信号不佳，换个姿势试试？")
                 }
                 self.vc.hideProgressDialog()
                 WifiLoginHelper.working = false
