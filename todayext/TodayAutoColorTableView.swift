adc41f2c24596f60cb59f55f1618894041ca947e
rikumi <riku.mi@outlook.com>
2017-01-03 17:41:11 +0800

    调整iOS9下widget分割线透明度

--- todayext/TodayAutoColorTableView.swift
+++ todayext/TodayAutoColorTableView.swift
@@ -14,7 +14,7 @@ class TodayAutoColorTableView: UITableView {
         if #available(iOSApplicationExtension 10.0, *) {
             separatorColor = .lightGray
         } else {
-            separatorColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0.3)
+            separatorColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0.1)
         }
     }
 }
